0d3a955726358646f7487b3f42a0bdc0bfe92edd
Dillon Skaggs <dillon@dillonskaggs.dev>
2026-01-29 15:46:55 -0600

    chore(ci): use base directory instead of next

--- .worker/build.sh
+++ .worker/build.sh
@@ -6,10 +6,10 @@ curl -L https://github.com/nativewrappers/NativeCodeGen/releases/latest/download
 chmod +x nativegen
 
 # Generate outputs
-mkdir -p dist/next dist/cfx
-./nativegen generate -i . -o dist/next/natives.json -f json
-./nativegen generate -i . -o dist/next/natives.bin -f proto
-curl -L https://raw.githubusercontent.com/nativewrappers/NativeCodeGen/main/src/NativeCodeGen.Core/Export/natives.proto -o dist/next/natives.proto
+mkdir -p dist dist/cfx
+./nativegen generate -i . -o dist/natives.json -f json
+./nativegen generate -i . -o dist/natives.bin -f proto
+curl -L https://raw.githubusercontent.com/nativewrappers/NativeCodeGen/main/src/NativeCodeGen.Core/Export/natives.proto -o dist/natives.proto
 
 # Download Cfx natives
 curl -L https://static.cfx.re/natives/natives.json -o dist/cfx/natives.json || true
