75e7d9f0d1663d17f45d9c91aa2edc26c7971a44
Dillon Skaggs <dillon@dillonskaggs.dev>
2026-02-06 17:40:03 -0600

    tweak: compress proto file

--- .worker/build.sh
+++ .worker/build.sh
@@ -8,8 +8,14 @@ chmod +x nativegen
 # Generate outputs
 mkdir -p dist dist/cfx
 ./nativegen generate -i . -o dist/natives.json -f json
-./nativegen generate -i . -o dist/natives.bin -f proto
+./nativegen generate -i . -o dist/natives.bin -f proto --compress
 curl -L https://raw.githubusercontent.com/nativewrappers/NativeCodeGen/main/src/NativeCodeGen.Core/Export/natives.proto -o dist/natives.proto
+
+# Headers for Cloudflare Pages - tell browsers the protobuf binary is gzip-encoded
+cat > dist/_headers << 'EOF'
+/natives.bin
+  Content-Encoding: gzip
+EOF
 
 curl -L https://gist.githubusercontent.com/fingaweg/2a7653c73daf985f73667e9c424cb624/raw/399fa4738522edcb92b05d55d32f80f5cc3150a1/scrCommand_dump_b1491.50 -o dist/addressDump || true
 
